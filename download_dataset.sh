aa5576aa65c81159a54c762570596f798fe0ddb7
mark9804 <9006264+Mark9804@users.noreply.github.com>
2025-07-01 16:19:38 +0900

    chore: add data source

--- download_dataset.sh
+++ download_dataset.sh
@@ -15,3 +15,16 @@ rm datasets/complete_ms_data.zip
 curl -L -o datasets/kaist-dataset.zip https://www.kaggle.com/api/v1/datasets/download/adlteam/kaist-dataset
 unzip datasets/kaist-dataset.zip -d datasets/KAIST
 rm datasets/kaist-dataset.zip
+
+# download masks
+
+if [ ! -d "datasets/masks" ]; then
+    mkdir datasets/masks
+fi
+
+if [ ! -d "datasets/masks/real" ]; then
+    mkdir datasets/masks/real
+fi
+
+curl -L -o datasets/masks/real/mask.mat https://github.com/mengziyi64/TSA-Net/raw/refs/heads/master/TSA_Net_realdata/Data/mask.mat
+curl -L -o datasets/masks/real/mask_3d_shift_uint16.mat https://github.com/mengziyi64/TSA-Net/raw/refs/heads/master/TSA_Net_realdata/Data/mask_3d_shift_uint16.mat
